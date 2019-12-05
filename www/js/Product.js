171841688bf418dd5a091b56bb97fa6c7a8795a9
Anton <anton.ohlin@hotmail.com>
2019-12-05 13:07:07 +0100

    product details restyle

--- www/js/Product.js
+++ www/js/Product.js
@@ -23,19 +23,16 @@ class Product {
 
   render() {
     $('main').removeClass('startsida');
-    $('main').html(/*html*/ `<section class="row">
-        <div class="col">
+    $('main').html(/*html*/ `
+    <section class="row mt-5">
+    <div class="col-8 col-md-6 col-lg-6">
+    <img class="img-fluid border border-primary animationImage" id="animationImage-${this.id}" src="${this.image}">
+    </div>
+        <div class="col-4 col-md-6 col-lg-6">
           <h1>${this.name}</h1>
-        </div>
-      </section>
-      <section class="row">
-        <div class="col-12 col-lg-9">
           <p>${this.description}</p>
-          <h4>${this.price}:-</p>
+          <h4 class="col-6">${this.price}:-</p>
           <button id="buy-button-${this.id}" class="btn rounded-0 btn-warning my-2">Köp</button>
-        </div>
-        <div class="col-12 col-lg-3 view overlay">
-        <img class="img-fluid border border-primary animationImage" id="animationImage-${this.id}" src="${this.image}">
         </div>
       </section>
     `);
@@ -43,17 +40,16 @@ class Product {
 
   renderInList() {
     return `
-      <div class="col-12 col-md-4 col-lg-3 mt-5">
+      <div class="col-12 col-md-4 col-lg-3 mt-2">
         <a class="productDisplay" href="#${this.slug}">
           <h4>${this.name}</h4>
           <div class="productOverlay"></div>
-          <section class="productImage">
+          <section class="productImage pb-1">
           <img class="img-fluid border border-primary displayImage" src="${this.image}">
           <img class="img-fluid border border-primary animationImage" id="animationImage-${this.id}" src="${this.image}">
           </section>
-          
           <h4>${this.price}:-</h4>
-          <button id="buy-button-${this.id}" class="btn rounded-0 float-right btn-warning my-2">Köp</button>
+          <button id="buy-button-${this.id}" class="btn rounded-0 float-right btn-warning">Köp</button>
         </a>
       </div>
     `
